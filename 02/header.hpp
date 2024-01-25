a0fec51c7ed997ff8ab68fea73a4999a3feb5eb1
Lana <115147586+ricorett@users.noreply.github.com>
2024-01-25 13:40:41 +0300

    fix 02 task

--- 02/header.hpp
+++ 02/header.hpp
@@ -7,12 +7,4 @@ struct bankDb {
   double balance;
 };
 
-void new_Balance(bankDb &bank_acc, double &newBalance) {
-  std::cout << "Введите введите новый баланс: ";
-  std::cin >> newBalance;
-  
-  bank_acc.balance = newBalance;
-  
-  std::cout << "Ваш счёт: " << bank_acc.name << ", " << bank_acc.id << ", "
-            << bank_acc.balance << std::endl;
-}
+void new_Balance(bankDb bank_acc, double &newBalance);
--- 02/main.cpp
+++ 02/main.cpp
@@ -10,8 +10,15 @@ int main() {
   std::cin >> bank_acc.name;
   std::cout << "Введите введите баланс: ";
   std::cin >> bank_acc.balance;
-
+  std::cout << "Введите введите новый баланс: ";
+  std::cin >> newBalance;
   new_Balance(bank_acc, newBalance);
 
   return 0;
+}
+void new_Balance(bankDb bank_acc, double &newBalance) {
+  bank_acc.balance = newBalance;
+  
+  std::cout << "Ваш счёт: " << bank_acc.name << ", " << bank_acc.id << ", "
+            << bank_acc.balance << std::endl;
 }
